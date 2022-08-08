c1c38c483b45771111f46d6a0249b1b33b89ecdb
Hend Emad <32018228@hti.edu.eg>
2022-08-08 21:14:33 +0200

    Delete test.py

--- Obstacles detection & Path planning/test.py
+++
@@ -1,6 +0,0 @@
-import obstacles_detection
-occupied_grids, planned_path = obstacles_detection.main("img_1.png")
-print ("Occupied Grids : ")
-print (occupied_grids)
-print ("Planned Path :")
-print (planned_path)
